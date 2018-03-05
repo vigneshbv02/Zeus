0552dbd5b06dc05aa059f85df4fe739068ad4a70
Ranjith Reynold <ranjithreynold75@gmail.com>
2018-03-05 19:24:37 +0530

    updated fragments for trip

---
+++ app/src/main/java/com/example/vignesh/zeus/send_message.java
@@ -0,0 +1,29 @@
+package com.example.vignesh.zeus;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class send_message extends Fragment {
+
+
+    public send_message() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_send_message, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/vignesh/zeus/trip_plan.java
@@ -0,0 +1,29 @@
+package com.example.vignesh.zeus;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class trip_plan extends Fragment {
+
+
+    public trip_plan() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trip_plan, container, false);
+    }
+
+}
